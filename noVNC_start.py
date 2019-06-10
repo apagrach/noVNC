a22d5608138338a2f67991d8ed59821958d0f414
ADAM <apagrach@gmail.com>
2019-06-10 10:05:26 -0400

    Added comments to startup/shutdown scripts

--- noVNC_start.py
+++ noVNC_start.py
@@ -5,6 +5,9 @@ import traceback
 import subprocess
 import getopt
 
+
+#Preapender is used to write a 'c' to the start of the log file for each device
+#this is used to prevent shutting down the server while another user still has a window open
 def line_prepender(filename, line):
     with open(filename, 'r+') as f:
         content = f.read()
@@ -12,7 +15,9 @@ def line_prepender(filename, line):
         f.write(line.rstrip('\r\n') + '\n' + content)
 
 
-
+#Command line args are used to specify the device
+#-h takes the ip address and VNC server port ex. 197.168.0.10:5900
+#-p takes the websocket port for the VNC server ex. 6080. each port number must be diffrent for each instance of noVNC
 opts, args = getopt.getopt(sys.argv[1:],"h:p:")
 for opt, arg in opts:
 	if opt in ("-h"):
@@ -20,8 +25,10 @@ for opt, arg in opts:
 	elif opt in ("-p"):
 		port=arg
 
+#A new file path for the log file is created based on the ips
 path = "/home/adam/noVNC/"+hmi.strip("\n")+"_PID.txt"
 
+# Popen si used to start the server. The PID of the server is writen to the log file
 cmd=['exec /home/adam/noVNC/utils/launch.sh --vnc ' + hmi + " --listen "+port]
 proc = subprocess.Popen(cmd,shell=True,preexec_fn=os.setsid)
 f = open(path, "a+")
--- noVNC_stop.py
+++ noVNC_stop.py
@@ -5,15 +5,23 @@ import traceback
 import subprocess
 import getopt
 
+#Command line args are used to specify the device
+#-h takes the ip address and VNC server port ex. 197.168.0.10:5900
+#-p takes the websocket port for the VNC server ex. 6080. each port number must be diffrent for each instance of noVNC
 opts, args = getopt.getopt(sys.argv[1:],"h:p:")
 for opt, arg in opts:
 	if opt in ("-h"):
 		hmi= arg
 	elif opt in ("-p"):
 		port=arg
+
+#The log file name and location is based on the Device IP and port		
 path = "/home/adam/noVNC/"+hmi.strip("\n")+"_PID.txt"
 f = open(path,"r+")
 
+
+#In the log file the number of lines with 'c' specifieys the number of current users
+#of that server. When a user closes thier session one 'c' is removed from the file
 line = f.readlines()
 flag = 0
 f.seek(0)
@@ -27,6 +35,8 @@ f.close
 f = open(path,"r+")
 line = f.readlines()
 print line
+
+#If all users are done with the server then the server will be shut down by killing the proccess
 if 'c\n' not in line:
 	for x in line:
 		try:
