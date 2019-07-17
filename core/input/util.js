2179a405ac2714e6322a3d12959c86cf08813c0d
ADAM <apagrach@gmail.com>
2019-07-17 08:25:48 -0400

    test

--- core/input/util.js
+++ core/input/util.js
@@ -72,8 +72,8 @@ export function getKey(evt) {
     if (evt.key !== undefined) {
         // IE and Edge use some ancient version of the spec
         // https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/8860571/
-        console.log('util.js lin 75 ')
-        console.log(evt.key.toString())
+        console.log('util.js lin 75 ');
+        console.log(evt.char);
         switch (evt.key) {
             case 'Spacebar': return ' ';
             case 'Esc': return 'Escape';
